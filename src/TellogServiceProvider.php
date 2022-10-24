39c0e58057d43a56e0f28b11e8f9df5821fa44b1
Александр Агакин <agakinalexandr@gmail.com>
2022-10-24 13:29:16 +0200

    initial

---
+++ src/TellogServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Alagakin\Tellog;
+
+use Illuminate\Support\ServiceProvider;
+
+class TellogServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+
+    }
+}
